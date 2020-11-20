37f2c807969e54b3b1674c4cd906afec19ee7447
JSMataKing <47906803+JSMataKing@users.noreply.github.com>
2020-11-20 00:49:29 -0800

    I finally got the words to frame symmetrically however, I am now running into the issue of a strange binary character appearing unprompted. I'll fix it soon

--- main.cpp
+++ main.cpp
@@ -22,9 +22,13 @@ void sentenceFramer(string sentence)
     bool wordLoopComplete;
     double firstWordLength;
     int incrementer;
-    double rowNumber;
-    string spacelessSentence;
-    string row;
+    double rowNumber = 0;
+    int inputLength = sentence.length();
+    char spacelessSentence[inputLength];
+    int rowLength = 0;
+    int rowRemainder = 0;
+    int sentenceLength = 0;
+    int counter = 0;
 
     // Verifier to cease loop when firstWordLength has been found
     wordLoopComplete = false;
@@ -48,55 +52,66 @@ void sentenceFramer(string sentence)
 
     // Loop to print first row of asterisks
     for (int index = 0; index < (firstWordLength + 2); index++)
-        cout << "*";
+        std::cout << "*";
     // Return line
-    cout << endl;
+    std::cout << endl;
 
     // Loop to create new sentence with no spaces
     for (int index = 0; index < sentence.length(); index++)
     {
-        if (sentence[index] != ' ')
-            spacelessSentence += sentence[index];
+        if (sentence[index] != ' ' && sentence[index] != NULL)
+        {
+            spacelessSentence[counter] = sentence[index];
+            counter++;
+        }
+        else if (sentence[index] == ' ' || sentence[index] == NULL)
+        {
+            counter = counter;
+        }
     };
 
+    sentenceLength = sizeof(spacelessSentence) / sizeof(spacelessSentence[0]);
+
     // Create the number of rows for the frame
-    rowNumber = spacelessSentence.length() / firstWordLength;
+    rowNumber = ceil(sentenceLength / firstWordLength);
 
     // Loop to display the sentence in a frame
-    for (int indexOne = 0; indexOne < ceil(rowNumber); indexOne++)
+    for (int indexOne = 0; indexOne < rowNumber; indexOne++)
     {
         // Print asterisk BEFORE the word
-        cout << "*";
+        std::cout << "*";
         // Print firstWordLength letters
         for (int indexTwo = incrementer; indexTwo < (firstWordLength * (indexOne + 1)); indexTwo++)
         {
-            if (spacelessSentence[indexTwo] != ' ')
-                row += spacelessSentence[indexTwo];
+            std::cout << spacelessSentence[indexTwo];
+            // add firstWordLength letters into a row
+            if (spacelessSentence[indexTwo] != ' ' && spacelessSentence[indexTwo] != NULL)
+            rowLength++;
         };
-        int rowRemainder = firstWordLength - row.length();
+        // create an int that holds a remainder
+        rowRemainder = firstWordLength - rowLength;
+        // if the rmainder is creater than 0
         if (rowRemainder > 0)
         {
-            cout << row;
+            // Loop to print asterisks in the empty row spots
             for (int index = 0; index < rowRemainder; index++)
             {
-                cout << "*";
+                std::cout << "*";
             }
         }
-        else
-        {
-            cout << row;
-        }
         // Print asterisk AFTER the word
-        cout << "*" << endl;
-        row.clear();
+        std::cout << "*" << endl;
+        // clear the contents inside of the row
+        rowLength = 0;
+        // add firstWordLength to the incremeneter
         incrementer += (firstWordLength);
     };
 
     // Loop to print last row of asterisks
     for (int index = 0; index < (firstWordLength + 2); index++)
-        cout << "*";
+        std::cout << "*";
     // Return line
-    cout << endl;
+    std::cout << endl;
 }
 
 // Purpose: Prompt user for a string to then input within sentenceFramer on execution
@@ -109,9 +124,10 @@ int main()
     prompt = "Please enter a single sentence (period optional) : ";
 
     // Display prompt
-    cout  << prompt;
+    std::cout  << prompt;
     // Input sentence
-    getline(cin, sentence);
+    sentence = "Hello world";
+    //getline(cin, sentence);
     // Execute sentenceFramer using inputted sentence
     sentenceFramer(sentence);
     return 0;
